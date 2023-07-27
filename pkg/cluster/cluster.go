5c8e2bb08ef50278c7f7557c0331e63e098c85fc
Matt Yaraskavitch <yaraskavitch.matt@gmail.com>
2023-07-26 21:32:12 -0400

    Log on conflict

--- pkg/cluster/cluster.go
+++ pkg/cluster/cluster.go
@@ -12,9 +12,18 @@ type Cluster struct {
 	ml *memberlist.Memberlist
 }
 
+type MemberlistHandler struct{}
+
+func (m MemberlistHandler) NotifyConflict(existing *memberlist.Node, added *memberlist.Node) {
+	log.Infof("%s(%s) and %s(%s) joined, are same node", existing, existing.Addr, added, added.Addr)
+}
+
 func (c Cluster) JoinCluster(listenPort int, bootstrapNodes []netip.AddrPort) {
 
+	handler := MemberlistHandler{}
+
 	mlConfig := memberlist.DefaultLocalConfig()
+	mlConfig.Conflict = handler
 	mlConfig.BindPort = listenPort
 
 	list, err := memberlist.Create(mlConfig)
